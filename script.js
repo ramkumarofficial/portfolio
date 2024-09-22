137a40a8bf06d0d608558d485af47b9da8363d81
RAMKUMAR K <ramkumar.it22@bitsathy.ac.in>
2024-09-22 13:20:12 +0530

    Major change

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+// Collapse navbar on mobile after clicking a link
+document.querySelectorAll('.navbar-nav a').forEach(anchor => {
+    anchor.addEventListener('click', function() {
+        if (window.innerWidth < 992) {
+            document.querySelector('.navbar-collapse').classList.remove('show');
+        }
+    });
+});
+
+// Shrink navbar on scroll
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        document.querySelector('header').classList.add('shrink');
+    } else {
+        document.querySelector('header').classList.remove('shrink');
+    }
+});
